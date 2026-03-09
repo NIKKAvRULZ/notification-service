74a5cb2ed7d5c5188ee0a27104c77c41d61a7716
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2026-03-09 21:50:51 +0530

    feat: Initialize the Notification Service Spring Boot application.

--- src/main/java/com/foodsystem/notification_service/NotificationServiceApplication.java
+++ src/main/java/com/foodsystem/notification_service/NotificationServiceApplication.java
@@ -3,7 +3,11 @@ package com.foodsystem.notification_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
